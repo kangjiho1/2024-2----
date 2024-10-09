fd3ab450da94b2b3fb61fa63a6500572b2e0bc1c
kangjiho1 <165993218+kangjiho1@users.noreply.github.com>
2024-10-09 21:16:43 +0900

    Create 10845.py

---
+++ hw 8차시/10845.py
@@ -0,0 +1,41 @@
+from collections import deque
+import sys
+
+data = deque([])
+
+input = sys.stdin.readline
+for _ in range(int(input())):
+    temp = input().split()
+
+    if len(temp) == 2:
+        data.append(temp[1])
+
+    else:
+        order = temp[0]
+
+        if order == 'pop':
+            if len(data):
+                print(data.popleft())
+
+            else:
+                print(-1)
+
+        elif order == 'size':
+            print(len(data))
+
+        elif order == 'empty':
+            print(0 if len(data) else 1)
+
+        elif order == 'front':
+            if len(data):
+                print(data[0])
+
+            else:
+                print(-1)
+
+        elif order == 'back':
+            if len(data):
+                print(data[-1])
+
+            else:
+                print(-1)
